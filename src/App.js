8e65cee043133dfc33b3ba34a973dc80d70ee11e
barisskann <bars_958@hotmail.com>
2023-01-03 20:20:02 +0300

    S6-G2

--- src/App.js
+++ src/App.js
@@ -5,37 +5,46 @@
 */
 
 // State hook u import edin
-import React from 'react';
+import React, { useState } from "react";
+import sahteVeri from "./sahte-veri";
+import AramaÇubuğu from "./bileşenler/AramaÇubuğu/AramaÇubuğu";
+import Gönderiler from "./bileşenler/Gönderiler/Gönderiler";
 
 // Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
 // sahteVeri'yi import edin
-import './App.css';
 
 const App = () => {
   // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
-	
-  const gonderiyiBegen = gonderiID => {
-    /*
-      Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
+  const [gönderi, setGonderi] = useState(sahteVeri);
+  const [arama, setArama] = useState("");
 
-      Uygulamanın durumu, React ağacının en üstünde bulunur, ancak iç içe geçmiş bileşenlerin stateleri değiştirememesi adil olmaz!
-      Bu fonksiyon, belirli bir gönderinin beğeni sayısını artırılmasına olanak sağlamak amacıyla iç içe geçmiş bileşenlere aktarılır.
-
-	  "setGonderi" yi çağırın ve state ine "posts.map" çağrısını iletin.
-      `map` içine iletilen callback aşağıdaki mantığı gerçekleştirir:
-        - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
-        - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
-     */
+  const gonderiyiBegen = (gonderiIxD) => {
+    setGonderi(
+      gönderi.map((item) => {
+        if (item.id === gonderiIxD) {
+          item.likes++;
+        }
+        return item;
+      })
+    );
   };
-
+  function filter(arama) {
+    const render = sahteVeri.filter((item) => item.username.includes(arama))
+    setGonderi(render)
+  }
   return (
-    <div className='App'>
-      {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
-      {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
+    <div className="App">
+      <AramaÇubuğu
+        gönderi={gönderi}
+        setArama={setArama}
+        arama={arama}
+        setGonderi={setGonderi}
+        filter={filter}
+      />
+      <Gönderiler gonderiler={gönderi} gonderiyiBegen={gonderiyiBegen} />
     </div>
   );
 };
-
 export default App;
--- src/bileşenler/AramaÇubuğu/AramaÇubuğu.js
+++ src/bileşenler/AramaÇubuğu/AramaÇubuğu.js
@@ -1,21 +1,27 @@
 // MUÜ için bu dosyayı değiştirmenize gerek yok
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faHeart, faCircle, faCompass } from '@fortawesome/free-regular-svg-icons';
+import sahteVeri from "../../sahte-veri";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import {
+  faHeart,
+  faCircle,
+  faCompass,
+} from "@fortawesome/free-regular-svg-icons";
 import "./AramaÇubuğu.css";
 
 const AramaÇubuğu = (props) => {
+  const { gönderi, setArama, arama, setGönderi, filter } = props;
+  const handleChange = (e) => {
+    filter(e.target.value);
+  };
   return (
     <div className="search-bar-wrapper">
       <div className="social">
         <FontAwesomeIcon icon={faInstagram} />
       </div>
       <form className="search-form">
-        <input
-          type="text"
-          placeholder="Arama"
-        />
+        <input onChange={handleChange} type="text" placeholder="Arama" />
       </form>
       <div className="social-wrapper">
         <div className="social">
